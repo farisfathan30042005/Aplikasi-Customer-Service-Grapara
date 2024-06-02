7063ab0ffd4e2dfb4ffa7fbfcbffc8c657a15eb3
MuhammadKautsar1 <148235804+MuhammadKautsar1@users.noreply.github.com>
2024-06-03 00:53:15 +0700

    First commit
    Tampilan FrontEnd Aplikasi Customer Service Grapara.

--- admin.js
+++
@@ -1,7 +0,0 @@
-// Fungsi untuk melakukan redirect ke halaman login
-function redirectToLogin() {
-    window.location.href = "login.html";
-}
-
-// Event listener untuk tombol "Admin Page"
-document.getElementById('adminPageBtn').addEventListener('click', redirectToLogin);
